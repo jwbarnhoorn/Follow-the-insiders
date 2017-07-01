1ff3c52aa982d5c19fa74150db2641bcdd8d33c4
jwbarnhoorn <jwbarnhoorn@gmail.com>
2017-07-01 20:01:04 +0200

    Add files via upload

--- SpiderBE.py
+++ SpiderBE.py
@@ -1,93 +1,80 @@
 # coding=utf-8
 from bs4 import BeautifulSoup   
-from urllib2 import urlopen
+import requests
 import sqlite3
-import datetime
+from datetime import datetime, timedelta
 import locale
 
 #Set local time to NL to parse date correctly.
-locale.setlocale(locale.LC_ALL,'nl_NL') #find correct localWin value at: https://docs.moodle.org/dev/Table_of_locales
+locale.setlocale(locale.LC_ALL,'Dutch_Netherlands.1252') #nl_NL for Linux.
 
 #Open DB connection
 conn = sqlite3.connect('insider_transactions.db', timeout=10)
 c = conn.cursor()
 
 #define some attributes
-url = "http://www.fsma.be/nl/Supervision/fm/ma/trans_bl/InsTrans.aspx"
-Base_url ="http://www.fsma.be" 
-
-Links_of_today = list() 
-Found_links = list() 
+url = "https://www.fsma.be/nl/transaction-search"
+Base_url = "https://www.fsma.be"
 New_links = list()
+headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36'}
 
+#Get date of yesterday in correct format
+today = datetime.now()
+yesterday = today - timedelta(days=1)
+yesterday_dt = yesterday.strftime('%d/%m/%Y')
+yesterday = str(yesterday_dt)
             
-#Check whether there are new links today: 
-#(1) load old links, (2) get links of today and (3) compare for new links
-#1 load old links
-c.execute("SELECT * FROM Old_links_BE")
-Old_links = [item[0] for item in c.fetchall()] 
-print("No. of old links: "+str(len(Old_links)))
+#Get new links of today
+r=requests.get(url, headers=headers)
+soup = BeautifulSoup(r.content, "html.parser")
 
-#2 Get links of today
-u = urlopen(url)
-try:
-   html = u.read().decode('utf-8')
-finally:
-   u.close()
-    
-soup = BeautifulSoup(html, "html.parser")
-Found_tags = soup.find('table', class_='dataOverview').findAll('a')
+All_links = soup.find('tbody').find_all('a')
+All_dates = soup.find('tbody').find_all('span', class_ = 'date-display-single')
 
-for tag in Found_tags:
-    tag = tag.get('href')
-    enriched_link = Base_url + tag
-    Links_of_today.append(enriched_link)
+i = 0
+for i in range(0,len(All_dates)):
+    if All_dates[i].getText() in yesterday:
+        link = Base_url + All_links[i].get('href')
+        New_links.append(link)
 
-# Remove duplicates out of the list.     
-Links_of_today = list(set(Links_of_today))      
-           
-#3 compare for new links                
-for link in Links_of_today:
-    if link not in Old_links:
-       New_links.append(link)    
-print("No. of new links: "+str(len(New_links)))
+print("No. of new links found: "+str(len(New_links)))
 
-#For all new links: spider the url and check if update is required
+#For all new links: spider the url and check if new insider transaction is found
+insider_transactions = 0
+Datum_meldingsplicht = yesterday_dt
 for link in New_links:  
-    u = urlopen(link)
-    try:
-        html = u.read().decode('utf-8')
-    finally:
-        u.close()
-
-    soup = BeautifulSoup(html, "html.parser")
-
-    #Fetch information
-    Datum_meldingsplicht = soup.find('span', text='Transactiedatum').findNext('span').getText()
-    Datum_meldingsplicht = datetime.datetime.strptime(Datum_meldingsplicht,"%d-%m-%Y")
-    Meldingsplichtige = soup.find('span', text='Naam meldplichtige').findNext('span').getText()
-    Uitgevende_instelling = soup.find('span', text='Emittent').findNext('span').getText()
-    Soort_effect = soup.find('span', text='Soort financieel instrument').findNext('span').getText()
-    Waarde_per_aandeel = soup.find('span', text='Prijs').findNext('span').getText()
-    Waarde_per_aandeel.strip()
+    r=requests.get(link, headers=headers)
+    soup = BeautifulSoup(r.content, "html.parser")
+    info = soup.find('div', class_ = 'ds-1col node node-manager-transactions node-view-full node-manager-transactions-full view-mode-full clearfix').find_all('div', class_ = 'field-item even')
+    #Fetch information  
+    Meldingsplichtige = info[1].getText()
+    Uitgevende_instelling = info[3].getText()
+    Soort_effect = info[4].getText()
+    Waarde_per_aandeel = info[10].getText()
+    Waarde_per_aandeel = Waarde_per_aandeel.strip()
     Waarde_per_aandeel = float(Waarde_per_aandeel.replace('.','').replace(',','.'))
-    Aantal_effecten = soup.find('span', text='Aantal financiële instrumenten').findNext('span').getText()
-    Aantal_effecten.strip()
-    Aantal_effecten = float(Aantal_effecten.replace(',','.'))
-    Totale_waarde = Aantal_effecten * Waarde_per_aandeel
-    Valuta = soup.find('span', text='Munt').findNext('span').getText()  
-    Soort_transactie = soup.find('span', text='Soort transactie').findNext('span').getText()
+    Aantal_effecten = info[9].getText()
+    Aantal_effecten = Aantal_effecten.strip()
+    Aantal_effecten = int(Aantal_effecten.replace('.',''))
+    Totale_waarde = info[11].getText()
+    Totale_waarde = Totale_waarde.strip()
+    Totale_waarde = float(Totale_waarde.replace('.','').replace(',','.'))
+    Valuta = info[8].getText()  
+    Soort_transactie = info[5].getText()
 
     #Check whether insider transaction conditions are met and if so store in database
-    if Totale_waarde > 10000 and (not "Verkoop" or "Vervreemding" in Soort_transactie) and ("aandeel" or "stock" in Soort_effect):
-               # INSERT the new record into the database.
-               c.execute("INSERT INTO relevant_transactions VALUES(?,?,?,?,?,?,?,?,?)",(Datum_meldingsplicht,Meldingsplichtige,Uitgevende_instelling,Soort_effect,Waarde_per_aandeel,Aantal_effecten,Totale_waarde,Valuta,'Belgium'))
-               conn.commit()
-
-#Write all crawled links to Old_links
-for link in New_links:
-    c.execute("INSERT INTO Old_links_BE VALUES(?)",(link,))       
-            
+    if Totale_waarde > 10000:
+        if ("Aankoop" or "Verwerving") in Soort_transactie:
+            if ("Aandeel" or "Stock") in Soort_effect:
+                # INSERT the new record into the database.
+                c.execute("INSERT INTO relevant_transactions VALUES(?,?,?,?,?,?,?,?,?)",(Datum_meldingsplicht,Meldingsplichtige,Uitgevende_instelling,Soort_effect,Waarde_per_aandeel,Aantal_effecten,Totale_waarde,Valuta,'Belgium'))
+                conn.commit()
+                # Add counter
+                insider_transactions += 1
+                
+print("No. of new insider transactions found: "+str(insider_transactions))
+              
 #Close DB
 conn.commit()     
 conn.close()
+
