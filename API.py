1bbb1ec28b76ce7c56dcbce8666c553edf73f56f
jwbarnhoorn <jwbarnhoorn@gmail.com>
2017-09-15 18:03:29 +0200

    Removed decimals by adding: u'¤ #,##0'

--- API.py
+++ API.py
@@ -25,7 +25,7 @@ class transactions:
         
         for item in transactions:
             item['Total_value'] = int(float(item['Total_value']))
-            item['Total_value'] = numbers.format_currency(item['Total_value'],item['Currency'], locale='es_US')   
+            item['Total_value'] = numbers.format_currency(item['Total_value'],item['Currency'], u'¤ #,##0')   
         
         return json.dumps(transactions)
 
